59a860be25427b347722585047ed4137bfec9ce9
PrasannaIITH <37734715+PrasannaIITH@users.noreply.github.com>
2019-02-21 11:18:03 +0530

    Add files via upload

---
+++ opencv-dep.sh
@@ -0,0 +1,11 @@
+# These are the dependencies required for the installation of opencv
+
+sudo apt-get update
+sudo apt-get install build-essential
+
+# Drives
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+
+# Optional
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+
